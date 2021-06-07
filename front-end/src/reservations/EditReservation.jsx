6a100dbe6b238ac3fd21ce96d39494428c97f725
Theodore Jones <t.jones0288@gmail.com>
2021-06-07 11:50:37 -0500

    Reservationform Component added!

--- front-end/src/reservations/EditReservation.jsx
+++ front-end/src/reservations/EditReservation.jsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from "react";
 import {useHistory, useParams} from "react-router-dom";
 import {formatPhoneNumber, updateReservation, readReservation} from "../utils/api";
 import {today} from "../utils/date-time"
-import ErrorAlert from "../layout/ErrorAlert";
+import ReservationForm from "./ReservationForm";
 
 export default function EditReservation() {
     const [error, setError] = useState(null);
@@ -81,103 +81,12 @@ export default function EditReservation() {
             
   }
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        {error && <ErrorAlert error={error} />}                      
-        <div className="form-group">
-          <label htmlFor="first_name">First Name:&nbsp;</label>
-          <input
-            name="first_name"
-            type="text"
-            placeholder="First Name"
-            className="form-control"
-            id="first_name"
-            value={formFields?.first_name}
-            required
-            onChange={handleChange}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="last_name">Last Name:&nbsp;</label>
-          <input
-            name="last_name"
-            type="text"
-            placeholder="last_name"
-            className="form-control"
-            id="last_name"
-            value={formFields?.last_name}
-            required
-            onChange={handleChange}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="mobile_number">Mobile Number:&nbsp;</label>
-          <input
-            type="tel"
-            name="mobile_number"
-            placeholder="XXX-XXX-XXXX"
-            className="form-control"
-            id="mobile_number"
-            value={formFields?.mobile_number}
-            required
-            onChange={phoneNumberFormatter}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="reservation_date">Date of Reservation:&nbsp;</label>
-          <input
-            type="date"
-            name="reservation_date"
-            placeholder="YYYY-MM-DD"
-            pattern="\d{4}-\d{2}-\d{2}"
-            className="form-control"
-            id="reservation_date"
-            value={formFields?.reservation_date}
-            required
-            onChange={handleChange}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="reservation_time">Time of Reservation:&nbsp;</label>
-          <input
-            type="time"
-            name="reservation_time"
-            placeholder="HH:MM"
-            pattern="[0-9]{2}:[0-9]{2}"
-            className="form-control"
-            id="reservation_time"
-            value={formFields?.reservation_time}
-            required
-            onChange={handleChange}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="people">Number In Party:&nbsp;</label>
-          <input
-            type="number"
-            name="people"
-            placeholder="Number in Party"
-            className="form-control"
-            id="people"
-            min="1"
-            required
-            value={formFields?.people}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <button type="submit" className="btn btn-primary mx-2">
-            Submit
-          </button>
-          <button
-            type="button"
-            className="btn btn-secondary"
-            onClick={history.goBack}
-          >
-            Cancel
-          </button>
-        </div>
-      </form>
-    </>
+    <ReservationForm
+      handleSubmit={handleSubmit}
+      handleChange={handleChange}
+      formFields={formFields}
+      errors={error}
+      phoneNumberFormatter={phoneNumberFormatter}
+    />
   );
 }
--- front-end/src/reservations/NewReservation.jsx
+++ front-end/src/reservations/NewReservation.jsx
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import { useHistory } from "react-router";
 import { today } from "../utils/date-time";
 import { createReservation, formatPhoneNumber } from "../utils/api";
-import ErrorAlert from "../layout/ErrorAlert";
+import ReservationForm from "./ReservationForm";
 
-export default function NewReservation({date, loadDashboard}) {
+export default function NewReservation({ date, loadDashboard }) {
   const [errors, setErrors] = useState(null);
   const [formFields, setFormFields] = useState({
     first_name: "",
@@ -58,115 +58,25 @@ export default function NewReservation({date, loadDashboard}) {
     const validDate = validateDate();
     if (validDate) {
       createReservation(formFields)
-        .then( history.push(`/dashboard?date=${formFields.reservation_date}`))
+        .then(() =>
+          history.push(`/dashboard?date=${formFields.reservation_date}`)
+        )
         .catch((err) => setErrors(err));
     }
-  } 
-  function handleChange({target}) {
+  }
+  function handleChange({ target }) {
     setFormFields({
       ...formFields,
       [target.name]: target.value,
-    })
-            
+    });
   }
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        {errors && <ErrorAlert error={errors} />}                      
-        <div className="form-group">
-          <label htmlFor="first_name">First Name:&nbsp;</label>
-          <input
-            name="first_name"
-            type="text"
-            placeholder="First Name"
-            className="form-control"
-            id="first_name"
-            value={formFields.first_name}
-            required
-            onChange={handleChange}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="last_name">Last Name:&nbsp;</label>
-          <input
-            name="last_name"
-            type="text"
-            placeholder="last_name"
-            className="form-control"
-            id="last_name"
-            value={formFields.last_name}
-            required
-            onChange={handleChange}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="mobile_number">Mobile Number:&nbsp;</label>
-          <input
-            type="tel"
-            name="mobile_number"
-            placeholder="XXX-XXX-XXXX"
-            className="form-control"
-            id="mobile_number"
-            value={formFields.mobile_number}
-            required
-            onChange={phoneNumberFormatter}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="reservation_date">Date of Reservation:&nbsp;</label>
-          <input
-            type="date"
-            name="reservation_date"
-            placeholder="YYYY-MM-DD"
-            pattern="\d{4}-\d{2}-\d{2}"
-            className="form-control"
-            id="reservation_date"
-            value={formFields.reservation_date}
-            required
-            onChange={handleChange}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="reservation_time">Time of Reservation:&nbsp;</label>
-          <input
-            type="time"
-            name="reservation_time"
-            placeholder="HH:MM"
-            pattern="[0-9]{2}:[0-9]{2}"
-            className="form-control"
-            id="reservation_time"
-            value={formFields.reservation_time}
-            required
-            onChange={handleChange}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="people">Number In Party:&nbsp;</label>
-          <input
-            type="number"
-            name="people"
-            placeholder="Number in Party"
-            className="form-control"
-            id="people"
-            min="1"
-            required
-            value={formFields.people}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <button type="submit" className="btn btn-primary mx-2">
-            Submit
-          </button>
-          <button
-            type="button"
-            className="btn btn-secondary"
-            onClick={history.goBack}
-          >
-            Cancel
-          </button>
-        </div>
-      </form>
-    </>
+    <ReservationForm
+      handleSubmit={handleSubmit}
+      handleChange={handleChange}
+      formFields={formFields}
+      errors={errors}
+      phoneNumberFormatter={phoneNumberFormatter}
+    />
   );
 }

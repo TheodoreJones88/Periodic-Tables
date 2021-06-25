3f6cfb2618e8f0f67001edf9f87a1e84240f9a6e
Theodore Jones <t.jones0288@gmail.com>
2021-06-25 12:35:39 -0500

    Updated filsd for proper deployment

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const path = require("path");
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL = "postgres://jnakfexy:fHaWqY45KxwDH-ey0gAgb6BS_oYsXzN0@queenie.db.elephantsql.com/jnakfexy",
   DATABASE_URL_DEVELOPMENT = "postgresql://postgres@localhost/postgres",
   DATABASE_URL_TEST = "postgresql://postgres@localhost/postgres",
   DATABASE_URL_PREVIEW = "postgresql://postgres@localhost/postgres",
